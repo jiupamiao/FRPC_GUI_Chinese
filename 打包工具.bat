bd4b0cb9a6b8db606c8832d55e1eb815e9080d6b
糯米茨 <143102889+nuomicici@users.noreply.github.com>
2025-06-21 12:20:12 +0800

    Update 打包工具.bat
    分离打包工具，把安装依赖库的命令作为另一个文件

--- 打包工具.bat
+++ 打包工具.bat
@@ -1,17 +1,3 @@
-
-pip install pyinstaller
-if errorlevel 1 (
-    echo error: Install pyinstaller failed!
-    goto :END
-)
-
-pip install PyQt5
-if errorlevel 1 (
-    echo error: Install PyQt5 failed!
-    goto :END
-)
-
-
 pyinstaller --onefile --name Tool_FRP_Non-official --icon=icon.ico app.pyw
 if errorlevel 1 (
     echo error: Build failed!
